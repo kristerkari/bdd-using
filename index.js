1f68acd0bbb01e1b7dcc4620c3d60b2d68d8446e
Krister Kari <krister.kari@webtehdas.fi>
2014-04-03 23:04:01 +0300

    Fixes

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-'use strict';
-
 function using(name, values, func) {
-  var oldIt = window.it;
+  var oldIt = it;
   for (var i = 0, len = values.length; i < len; i++) {
-    window.it = function() {
+    it = function() {
       var args = Array.prototype.slice.call(arguments);
       args[0] += ' (with "' + name + '" using "' + values[i] + '")';
       oldIt.apply(this, args);
     };
-    func.call(this, value);
+    func.call(this, values[i]);
   }
-  window.it = oldIt;
+  it = oldIt;
 }
 
 module.exports = using;
